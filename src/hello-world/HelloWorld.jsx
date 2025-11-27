f1a9d0db25642cc3b8516050c1ecb836fd3fd0df
revansanreigoputra <revanasanreigo1@gmail.com>
2025-11-27 14:12:50 +0700

    Hello World

---
+++ src/hello-world/HelloWorld.jsx
@@ -0,0 +1,10 @@
+export default function HelloWorld() {
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <p>Welcome to my first React component!</p>
+        </div>
+    );
+}
+
+// export default HelloWorld;
