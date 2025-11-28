48323c0c8a8b0fdf7e753464c2c201622947abb7
revansanreigoputra <revanasanreigo1@gmail.com>
2025-11-28 09:07:33 +0700

    props

--- src/hello-world/HelloWorld.jsx
+++ src/hello-world/HelloWorld.jsx
@@ -1,55 +1,14 @@
-// export default function HelloWorld() {
-//     return (
-//         <div>
-//             <h1>Hello World</h1>
-//             <p>Welcome to my first React component!</p>
-//         </div>
-//     );
-// }
-
-// export default HelloWorld;
-
 // multiple components in one file / pemecahan function
 export default function HelloWorld() {
     return (
         <div>
-            <HeaderHelloWorld />
+            <HeaderHelloWorld text="Hello World!" />
             <ParagraphHelloWorld />
         </div>
     );
 }
 
-// function HeaderHelloWorld() {
-//     return (
-//         <h1>Hello World</h1>
-//     )
-// }
-
-// function ParagraphHelloWorld() {
-//     return (
-//         <p>Welcome to my first React component!</p>
-//     )
-// }
-
-
-// Javascript di JSX
-// function HeaderHelloWorld() {
-//     const text = "Hello World";
-//     return (
-//         <h1>{text.toUpperCase()}</h1>
-//     )
-// }
-
-// function ParagraphHelloWorld() {
-//     const text = "Welcome to my first React component!";
-//     return (
-//         <p>{text.toLowerCase()}</p>
-//     )
-// }
-
-// Kurung kurawal double {{}} di JSX
-function HeaderHelloWorld() {
-    const text = "Hello World";
+function HeaderHelloWorld({text = "Ups, Lupa Kasih Teks!"  }) {
     return (
         <h1 style={{
             color: "blue",
