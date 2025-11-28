5f45977ac6d8e92488d9888e1750a85ee9a6dc6d
revansanreigoputra <revanasanreigo1@gmail.com>
2025-11-28 08:30:56 +0700

    Multiple Component atau Pemecahan Function

--- src/hello-world/HelloWorld.jsx
+++ src/hello-world/HelloWorld.jsx
@@ -1,10 +1,33 @@
+// export default function HelloWorld() {
+//     return (
+//         <div>
+//             <h1>Hello World</h1>
+//             <p>Welcome to my first React component!</p>
+//         </div>
+//     );
+// }
+
+// export default HelloWorld;
+
+// multiple components in one file / pemecahan function
 export default function HelloWorld() {
     return (
         <div>
-            <h1>Hello World</h1>
-            <p>Welcome to my first React component!</p>
+            <HeaderHelloWorld />
+            <ParagraphHelloWorld />
         </div>
     );
 }
 
-// export default HelloWorld;
+
+function HeaderHelloWorld() {
+    return (
+        <h1>Hello World</h1>
+    )
+}
+
+function ParagraphHelloWorld() {
+    return (
+        <p>Welcome to my first React component!</p>
+    )
+}
