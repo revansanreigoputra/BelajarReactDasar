ba194c3ca306735ce318f3ea91431d9c93e60472
revansanreigoputra <revanasanreigo1@gmail.com>
2025-11-30 09:24:50 +0700

    collection component use key

--- src/todolist/TodoList.jsx
+++ src/todolist/TodoList.jsx
@@ -3,36 +3,46 @@ import Todo from "./Todo";
 export default function TodoList() {
     const data = [
         {
+            id: 0,
             text: "Belajar React Dasar",
             isCompleted: true,
             isDeleted: true
 
         },
         {
+            id: 1,
             text: "Membuat Project React Pertama",
             isCompleted: true,
             
         },
         {
+            id: 2,
             text: "Deploy Project React",
             isCompleted: false,
             
         },
         {
+            id: 3,
             text: "Bersenang-senang dengan React",
+            isCompleted: false,
+            
+        }
+        ,
+        {
+            id: 4,
+            text: "Membaca Dokumentasi React",
             isCompleted: false,
             
         }
     ]
 
-    const todos = data.map((todo) => (
-        <Todo {...todo}/>
-    ))
     return (
         <div>
             <h2>Todo List</h2>
             <ul>
-                {todos}
+                {data.map((todo) => (
+                    <Todo key={todo.id} {...todo} />
+                ))}
             </ul>
         </div>
     )
