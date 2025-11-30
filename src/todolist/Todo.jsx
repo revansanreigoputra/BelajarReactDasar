6f7931c1574f65cf1c74a908321a7cd49df1575f
revansanreigoputra <revanasanreigo1@gmail.com>
2025-11-30 08:55:53 +0700

    (conditional) null component

--- src/todolist/Todo.jsx
+++ src/todolist/Todo.jsx
@@ -1,5 +1,7 @@
-export default function Todo({text, isCompleted}) {
-    if (isCompleted) {
+export default function Todo({text, isCompleted, isDeleted=false}) {
+    if(isDeleted) {
+        return null;
+    }else if (isCompleted) {
         return (
             <li>
                 <del>{text}</del>
--- src/todolist/TodoList.jsx
+++ src/todolist/TodoList.jsx
@@ -3,7 +3,7 @@ import Todo from "./Todo";
 export default function TodoList() {
     return (
         <ul>
-            <Todo text="Belajar React Dasar" isCompleted={true} />
+            <Todo text="Belajar React Dasar" isCompleted={true} isDeleted={true}/>
             <Todo text="Membuat Project React Pertama" isCompleted={false} />
             <Todo text="Deploy Project React" isCompleted={false} />
             <Todo text="Bersenang-senang dengan React" isCompleted={false} />
