3ad96076bd5ea217d31f2097c0832f48b31664ca
revansanreigoputra <revanasanreigo1@gmail.com>
2026-01-13 16:20:36 +0700

    Context dan State

--- src/profile/ProfileApp.jsx
+++ src/profile/ProfileApp.jsx
@@ -1,12 +1,18 @@
 import { ProfileContext } from "./ProfileContext";
 import Profile from "./Profile";
 import ProfileAddress from "./ProfileAddress";
+import ProfileForm from "./ProfileForm";
+import { useState } from "react";
 
 export default function ProfileApp() {
+
+    const [name, setName] = useState("Dregotzzz");
+
     return (
         <>
-            <ProfileContext.Provider value="Dregotzzz">
+            <ProfileContext.Provider value={name}>
                 <h1>Profile Application</h1>
+                <ProfileForm name={name} setName={setName} />
                 <Profile />
                 <ProfileAddress />
             </ProfileContext.Provider>
